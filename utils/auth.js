d62189ba41cb39fd1a3e950925fcca4db3c2f8bb
David Mark <dm0538632087@gmail.com>
2023-02-02 01:57:15 +0200

    added changes to auth url

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = 'AIzaSyDrRl6Lje6FM1mgavZcu_CUowsEZRYGttg';
+const API_KEY = '[FIREBASE_API_KEY]';
 
 async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
